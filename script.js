52db3e9729bdb3ded0a86c7270034fefc2ddef83
jucelio silva <jucelio94silva@gmail.com>
2024-04-04 13:37:12 -0300

    Landing page-Iphone

---
+++ script.js
@@ -0,0 +1,9 @@
+function imagem(e) {
+    document.querySelector('#phone').src = e;
+}
+
+function circle(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+
+}
